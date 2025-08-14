971b2824b4f1ad9c3bd74eb2d5f711a0f610e2a3
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-14 09:46:53 +0600

    Problem-02 Clean & Capitalize Characters

---
+++ Problem-02 Clean & Capitalize Characters.js
@@ -0,0 +1,21 @@
+function onlyCharacter(str) {
+    //todo ইনপুট চেক করবো
+    if (typeof str !== 'string') {
+        return "Invalid";
+    }
+
+    //todo স্পেস বাদ দিয়ে বড় হাতের অক্ষরে রূপান্তর
+    let result = str.replaceAll(" ", "").toUpperCase();
+
+    //todo রিটার্ন করবো
+    return result;
+}
+
+
+//todo Example Test:
+console.log(onlyCharacter("  h e llo wor   ld"));       //* HELLOWORLD
+console.log(onlyCharacter("Cy   bar- At  tac k  "));    //* CYBAR-ATTACK
+console.log(onlyCharacter(" ha ck m e 1 @ru.c  n  "));  //* HACKME1@RU.CN
+console.log(onlyCharacter("Serv er : : Do wn"));        //* SERVER::DOWN
+console.log(onlyCharacter(["hack", "me"]));             //* Invalid
+console.log(onlyCharacter(true));                       //* Invalid
