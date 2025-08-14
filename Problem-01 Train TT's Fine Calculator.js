d11517c4fddc72d7c8a324563220df14b4d24c1d
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-14 10:06:36 +0600

    Problem-01 Train TT's Fine Calculator

--- Problem-01 Train TT's Fine Calculator.js
+++ Problem-01 Train TT's Fine Calculator.js
@@ -5,8 +5,8 @@ function totalFine(fare) {
     }
 
     //todo জরিমানা হিসাব
-    let surcharge = fare * 20 / 100; // ২০% সারচার্জ
-    let serviceCharge = 30; // সার্ভিস চার্জ
+    let surcharge = fare * 20 / 100;    // ২০% সারচার্জ
+    let serviceCharge = 30;            // সার্ভিস চার্জ
     let total = fare + surcharge + serviceCharge;
 
     //todo মোট জরিমানা রিটার্ন
@@ -16,12 +16,11 @@ function totalFine(fare) {
 console.log(totalFine(200));
 
 
-
 //todo Example Test:
-console.log(totalFine(200)); //* 270
-console.log(totalFine(0));   //* Invalid
-console.log(totalFine(50));  //* 90
-console.log(totalFine(552)); //* 692.4
-console.log(totalFine(-35)); //* Invalid
-console.log(totalFine("65")); //* Invalid
+console.log(totalFine(200));        //* 270
+console.log(totalFine(0));         //* Invalid
+console.log(totalFine(50));       //* 90
+console.log(totalFine(552));     //* 692.4
+console.log(totalFine(-35));    //* Invalid
+console.log(totalFine("65"));  //* Invalid
 console.log(totalFine("Gorib tai ticket katinai")); //* Invalid
